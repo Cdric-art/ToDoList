83d328a4e65bef06ee150ec5282a7c1ed5ed7c6f
Cdric-art <cedric.evrard@me.com>
2023-06-27 01:19:50 +0200

    feat : created todo and save in the db

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class NewItemViewViewModel: ObservableObject {
     @Published var title: String = ""
@@ -13,7 +15,19 @@ class NewItemViewViewModel: ObservableObject {
     @Published var showAlert: Bool = false
     
     func save() {
+        guard canSave else { return }
         
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(id: newId, title: title, dueDate: dueDate.timeIntervalSince1970, createdDate: Date().timeIntervalSince1970, isDone: false)
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
