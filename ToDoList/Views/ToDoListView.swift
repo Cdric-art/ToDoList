fd18ce85a80913d3ec074dbf580c6668c269680c
Cdric-art <cedric.evrard@me.com>
2023-06-26 22:49:39 +0200

    Feat: Navigation List ToDo and Profil. View for add new todo

---
+++ ToDoList/Views/ToDoListView.swift
@@ -0,0 +1,43 @@
+//
+//  ToDolListItemsView.swift
+//  ToDoList
+//
+//  Created by Cédric Evrard on 22/06/2023.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    @StateObject var viewModel = ToDoListViewViewModel()
+    
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+            .navigationTitle("To Do List")
+            .toolbar {
+                Button {
+                    viewModel.showingNewItemView.toggle()
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $viewModel.showingNewItemView) {
+                NewItemView(newItemPresented: $viewModel.showingNewItemView)
+            }
+        }
+    }
+}
+
+struct ToDoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ToDoListView(userId: "")
+    }
+}
